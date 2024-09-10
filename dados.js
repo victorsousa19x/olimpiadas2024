b76f84e36a998ddd8cba57352b770fd846c3b88a
victorsousa19x <victorosousa.ol@gmail.com>
2024-09-09 22:17:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Ela conquistou diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances incríveis. Ela foi a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ouro"
+    },
+    {
+        titulo: "Tatiana Weston-Webb",
+        descricao: "Tatiana Weston-Webb é uma surfista profissional brasileira, conhecida por sua habilidade e estilo únicos. Ela representa o Brasil em diversas competições internacionais e conquistou diversos títulos importantes.",
+        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb",
+        tags: "surf surfista"
+    }
+];
